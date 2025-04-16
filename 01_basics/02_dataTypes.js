2db8bae7f6f27fa0fa3a935c8edcf7b27b9121f8
SHRIRAM RADHESHAM SONWANE <sonwaneshriram2@gmail.com>
2025-04-16 16:13:39 +0000

    datatypes-summary

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert(3+3)//we are using nodejs , not browser
+
+console.log(3
+    +3);//code readability that should be high
+console.log('shriram')
+
+
+let name= "SHriram"
+let age = 22
+let isLoggedIn= false
+let state;
+
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean=> true/false
+
+//null => standalone value
+//undefined=>
+//symbol => unique
+
+
+//object 
+
+
+console.log(typeof undefined);
+
+
