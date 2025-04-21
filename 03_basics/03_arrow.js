f925d59d3ce224b8229a1de00d1409c90d5e7f0b
SHRIRAM RADHESHAM SONWANE <sonwaneshriram2@gmail.com>
2025-04-21 14:33:06 +0000

    main

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -15,13 +15,43 @@ const user = {
 
 // console.log(this);
 
-function chai(){
-    let username ="shriram"
-    console.log(this);
+// function chai(){
+//     let username ="shriram"
+//     console.log(this.username);
     
-}
+// }
 
-chai()
+// chai()
+// const chai= function() {
+//     let username ="shriram"
+//     console.log(this.username);
+    
+// }
+
+// chai()
 
 
+// const chai= () => {
+//     let username ="shriram"
+//     // console.log(this.username);
+//     console.log(this);
+    
+// }
+
+// chai()
+
+// const addtwo =(num1,num2)=>{
+//     return num1+num2
+// }
+// console.log(addtwo(3,6));
+
+// const addtwo =(num1,num2)=> num1+num2
+
+// const addtwo =(num1,num2)=> (num1+num2) //implicit return
+
+const addtwo = (num1,num2)=> ({username:"Shriram"})
+console.log(addtwo(3,6));
+
+const myArray = [2,5,3,7,8]
+myArray.forEach()
 
