7fdaa730f69ba8b8bc96600ca63ee8f59c6db3cc
SHRIRAM RADHESHAM SONWANE <sonwaneshriram2@gmail.com>
2025-06-15 17:40:53 +0000

    main.js

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+// let index = 0;
+// while (index <=10) {
+//     console.log(`The  Number of index is${index}`);
+//     // index ++;
+//     index=index +2;
+// }
+
+let myArray =['Shriram','Onkar','Bheem']
+let arr=0
+while (arr < myArray.length) {
+    // console.log(`My array is ${myArray[arr]}`);
+    arr= arr + 1;
+    
+}
+
+// let score=1
+let score=11
+do {
+   console.log(`Score is ${score}`);
+    score++;
+} while (score< 10);
