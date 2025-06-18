e37ebcab5b5cb407a181c7beaa270cb7efc972ff
SHRIRAM RADHESHAM SONWANE <sonwaneshriram2@gmail.com>
2025-06-18 10:22:29 +0000

    main.js

---
+++ 05_iterations/four.js
@@ -0,0 +1,27 @@
+const myobject = {
+    js: 'javascript',
+    cpp:'c++',
+    jp:'java Programming',
+    swift:'swift by apple',
+    rb:'rubby',
+}
+for (const key in myobject) {
+// console.log(key);
+// console.log(myobject[key]);
+//console.log(`${key} shortcut for is ${myobject[key]}`);
+}
+
+const Programming =["js","rb","py","java","cpp"]
+for (const key in Programming) {
+    // console.log(Programming[key]);
+}
+
+
+const map = new Map()
+map.set("IN","India")
+map.set("USA","United State America")
+map.set("SA","South Africa")
+for (const key in map) {
+    console.log();
+    
+}
